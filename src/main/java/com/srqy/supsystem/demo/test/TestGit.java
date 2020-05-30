a68cf97aadf7af64984998b2fdf3feac4d62722f
yinwenwei <1043936421@qq.com>
2020-05-30 09:53:18 +0800

    yww test add

---
+++ src/main/java/com/srqy/supsystem/demo/test/TestGit.java
@@ -0,0 +1,15 @@
+package com.srqy.supsystem.demo.test;
+
+/**
+ * @Author yww
+ * @CreateTime 2020-05-30
+ */
+public class TestGit {
+    public static void main(String args[]){
+        System.out.println("柯伟老叼快来整合");
+
+        //你把Integer 改String
+        Integer count = 0;
+        System.out.println(count);
+    }
+}
